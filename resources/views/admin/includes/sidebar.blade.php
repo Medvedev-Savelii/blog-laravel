5602e6ae3aee13967e0c8fcdd48be9a16f9b3589
sava <savamedvedev@gmail.com>
2022-06-14 11:05:46 +0800

    seven-commit

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,37 @@
+
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon far fa-image"></i>
+                    <p>
+                        Категории
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon far fa-image"></i>
+                    <p>
+                        Теги
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon far fa-image"></i>
+                    <p>
+                        Посты
+                    </p>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+    <!-- /.sidebar -->
+</aside>

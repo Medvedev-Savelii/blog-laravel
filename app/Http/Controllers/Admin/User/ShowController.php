b9b8b0777aa5bbd54ea08a3afb65c3ee7b263eb3
sava <savamedvedev@gmail.com>
2022-06-17 10:00:39 +0800

    19-commit

--- app/Http/Controllers/Admin/User/ShowController.php
+++ app/Http/Controllers/Admin/User/ShowController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Admin\User;
 use App\Http\Controllers\Controller;
-use App\Models\Category;
 use App\Models\User;
 
 
--- app/Http/Controllers/Admin/User/UpdateController.php
+++ app/Http/Controllers/Admin/User/UpdateController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers\Admin\User;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\Category\UpdateRequest;
-use App\Models\Category;
+use App\Http\Requests\Admin\User\UpdateRequest;
 use App\Models\User;
 
 
